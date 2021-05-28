97672f5564cba2b1b5eb26945b415ffb75bab9f5
Letícia Kojima <leticia.missaki@hotmail.com>
2021-05-28 20:56:54 -0300

    Tela de Imposto - Aula 08

--- src/CalculoImpostoPF.java
+++ src/CalculoImpostoPF.java
@@ -2,6 +2,63 @@
 public class CalculoImpostoPF {
 
 	public static void main(String[] args) {
+		
+		String nome = "Flávio Moreni";
+		double salarioBruto = 20000;
+		double irRetido = 1;
+		double salarioLiquidoSemIr = 19999; 
+		double aliquota = 15; 
+		double inssRetido = 1;
+		double salarioLiquidoTotal = 19998;
+				
+		
+		//imprimirTelaInicial();
+		
+		imprimirResultado(nome, salarioBruto, irRetido, salarioLiquidoSemIr, aliquota, inssRetido, salarioLiquidoTotal);
+		
+	}
+
+	public static void imprimirResultado(
+			String nome, 
+			double salarioBruto, 
+			double irRetido, 
+			double salarioLiquidoSemIr, 
+			double aliquota, 
+			double inssRetido, 
+			double salarioLiquidoTotal) {
+		System.out.println("******************************************************************");
+		System.out.println("******************************************************************");
+		System.out.println("**");
+		System.out.println("**     	RESULTADO");
+		System.out.println("**");
+		System.out.println("**	   FUNCIONÁRIO: " + nome);
+		System.out.println("******************************************************************");
+		System.out.println("******************************************************************");
+		System.out.println("**");
+		System.out.println("**   IMPOSTO DE RENDA");
+		System.out.println("**");
+		System.out.println("**	 SALÁRIO BRUTO: "+ salarioBruto);
+		System.out.println("**	IMPOSTO RETIDO: " + irRetido + " BRL");
+		System.out.println("**	   SALÁRIO LÍQ: " + salarioLiquidoSemIr + " BRL");
+		System.out.println("**");
+		System.out.println("******************************************************************");
+		System.out.println("******************************************************************");
+		System.out.println("**");
+		System.out.println("**   PREVIDÊNCIA");
+		System.out.println("**");
+		System.out.println("**	 	  ALIQUOTA: " + aliquota + " %");
+		System.out.println("**	IMPOSTO RETIDO: " + inssRetido + " BRL");
+		System.out.println("**");
+		System.out.println("******************************************************************");
+		System.out.println("******************************************************************");
+		System.out.println("**");
+		System.out.println("**   SALÁRIO LIQUIDO: " + salarioLiquidoTotal + " BRL");
+		System.out.println("**");
+		System.out.println("******************************************************************");
+		System.out.println("******************************************************************");
+	}
+
+	public static void imprimirTelaInicial() {
 		System.out.println("******************************************************************");
 		System.out.println("******************************************************************");
 		System.out.println("**");
@@ -16,54 +73,6 @@ public class CalculoImpostoPF {
 		System.out.println("**");
 		System.out.println("******************************************************************");
 		System.out.println("******************************************************************");
-
-		
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("calculando ...");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		
-		
-		System.out.println("******************************************************************");
-		System.out.println("******************************************************************");
-		System.out.println("**");
-		System.out.println("**     	RESULTADO");
-		System.out.println("**");
-		System.out.println("**	   FUNCIONÁRIO: Antonio Coutinho");
-		System.out.println("******************************************************************");
-		System.out.println("******************************************************************");
-		System.out.println("**");
-		System.out.println("**   IMPOSTO DE RENDA");
-		System.out.println("**");
-		System.out.println("**	 SALÁRIO BRUTO: 8.000,00 BRL");
-		System.out.println("**	IMPOSTO RETIDO: 1.123,84 BRL");
-		System.out.println("**	   SALÁRIO LÍQ: 6.876,16 BRL");
-		System.out.println("**");
-		System.out.println("******************************************************************");
-		System.out.println("******************************************************************");
-		System.out.println("**");
-		System.out.println("**   PREVIDÊNCIA");
-		System.out.println("**");
-		System.out.println("**	 	  ALIQUOTA: 11 %");
-		System.out.println("**	IMPOSTO RETIDO: 880,00 BRL");
-		System.out.println("**");
-		System.out.println("******************************************************************");
-		System.out.println("******************************************************************");
-		System.out.println("**");
-		System.out.println("**   SALÁRIO LIQUIDO: 5.996,16 BRL");
-		System.out.println("**");
-		System.out.println("******************************************************************");
-		System.out.println("******************************************************************");
-		
 	}
 
 }
